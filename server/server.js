a081bfe260818e001874ca6dad742fcef3c87978
Greg <gvorvik@gmail.com>
2018-01-17 17:37:40 -0600

    Add GET todos route

--- server/server.js
+++ server/server.js
@@ -7,15 +7,29 @@ var {User} = require('./models/user');
 
 var app = express();
 
+//app.use takes and configures middleware
+//body-parser takes json data and converts it to an object
+//I sent the json that is converted via Postman
 app.use(bodyParser.json());
 
+//Method to send todos to server
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
 
+    //saves model to database
     todo.save().then((doc) => {
         res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+//Gets all existing todos
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
     }, (e) => {
         res.status(400).send(e);
     });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,20 +4,27 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+//Testing lifecycle method running before each test case
 beforeEach((done) => {
     Todo.remove({}).then(() => {
         done();
     });
 });
 
+//describe block to organize it statements during test
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
         var text = 'Test todo text';
 
+        //this is using supertest and expect modules FYI
+        //Look at their documentation online to learn more
         request(app)
             .post('/todos')
+            //new send method due to sending in data via todo
             .send({text})
             .expect(200)
+            //ensures response text equals the text we sent in
             .expect((res) => {
                 expect(res.body.text).toBe(text);
             })
@@ -26,6 +33,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
+                //fetches all the todos
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
@@ -37,6 +45,7 @@ describe('POST /todos', () => {
     it('should not create todo with invalid body data', (done) => {
         request(app)
             .post('/todos')
+            //sends in invalid data
             .send({})
             .expect(400)
             .end((err, res) => {
